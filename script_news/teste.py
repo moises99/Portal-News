90584f90c210464ad83b1bfe86388267a29f2dcb
moises99 <moisesp.silva@outlook.com.br>
2026-08-19 03:55:56 -0300

    revertendo alterações

--- script_news/teste.py
+++ script_news/teste.py
@@ -2,12 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
+
+# Firefox instalado manualmente
+options.binary_location = "/opt/firefox/firefox"
+
+# VM sem interface gráfica
 options.add_argument("--headless")
 
 driver = webdriver.Firefox(options=options)
 
 driver.get("https://www.google.com")
 
-print(driver.title)
+print("Título:", driver.title)
 
 driver.quit()
