5a4f9c6162ae7e913305126d4d6b3e290885614d
moises99 <moisesp.silva@outlook.com.br>
2026-08-19 01:09:56 -0300

    mudanças

--- news/settings.py
+++ news/settings.py
@@ -115,11 +115,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR / 'base_static',)
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
