edbe4d678e6bcde18f5d4fe3df82b0f7e35aacd7
moises99 <moisesp.silva@outlook.com.br>
2026-08-26 23:40:26 -0300

    Ajustes para vercel

--- news/wsgi.py
+++ news/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news.settings')
 
 application = get_wsgi_application()
+
