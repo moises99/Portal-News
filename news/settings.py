38bba509e19a6c316b7984af9d0cddd29ec9e7a7
moises99 <moisesp.silva@outlook.com.br>
2026-08-26 23:30:02 -0300

    Ajustes para vercel

--- news/settings.py
+++ news/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6)e!cotd!vlja@lacgybq_05uh!w#@-z_0s+s838yrkbb5dcb9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
