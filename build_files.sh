5eeb1aa0e3fcd8b49bb75ef2ab9c2aa7eb9d14c4
moises99 <moisesp.silva@outlook.com.br>
2026-08-27 01:55:04 -0300

    Ajustes para vercel

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
-echo "Instalando dependências..."
-pip install -r requirements.txt
-
 echo "Executando migrações do banco..."
-python3.14 manage.py migrate --noinput
+python3.12 manage.py migrate --noinput
 
 echo "Coletando arquivos estáticos..."
-python3.14 manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
