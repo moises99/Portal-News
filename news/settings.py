904e98d4e94b466c0b8c902ac8f20bd4e9f6440b
moises99 <moisesp.silva@outlook.com.br>
2026-09-11 02:52:10 -0300

    debug:false

--- news/settings.py
+++ news/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6)e!cotd!vlja@lacgybq_05uh!w#@-z_0s+s838yrkbb5dcb9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['portal-news-navy.vercel.app','*']
 
